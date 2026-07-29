1192c775a3d3110ba4da91cb771f38738b809c6d
Manan Awasthi <152317038+mananawasthi56@users.noreply.github.com>
2026-07-29 22:24:27 +0530

    Time: 43 ms (53.36%), Space: 78.8 MB (19.31%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-               int freq[26] = {0};
-
-        // frequency count
-        for (char ch : s) {
-            freq[ch - 'a']++;
+               int fre[26] = {};
+               for(char ch : s){
+                fre[ch - 'a']++;
+               }
+    string left = "";
+    char midd = '\0';
+    for( int i = 0 ;i<26 ; i++){
+        left += string(fre[i] / 2 , 'a' + i);
+        if(fre[i] % 2 == 1){
+            midd = 'a' + i;
         }
-
-        string left = "";
-        char middle = '\0';
-
-        // a to z -> lexicographically smallest
-        for (int i = 0; i < 26; i++) {
-
-            // half characters left side me
-            left += string(freq[i] / 2, 'a' + i);
-
-            // odd frequency character middle me
-            if (freq[i] % 2 == 1) {
-                middle = 'a' + i;
-            }
-        }
-
-        string right = left;
-        reverse(right.begin(), right.end());
-
-        if (middle != '\0') {
-            return left + middle + right;
-        }
-
-        return left + right;
     }
-        
+        string right = left;
+        reverse(right.begin() , right.end());
+        if(midd != '\0'){
+            return left + midd + right;
+        }
+        return left + right;
+
+    }
+
+
+     
     
 };
